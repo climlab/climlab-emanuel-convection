fc5805951091f28f666607426ef4249b44d4a197
Brian Rose <brose@albany.edu>
2022-04-07 11:01:08 -0400

    First commit of new stand-alone Emanuel convection driver

---
+++ README.md
@@ -0,0 +1,63 @@
+# climlab-emanual-convection
+
+Brian Rose, University at Albany
+
+## About
+
+This is a stand-alone Python wrapper for the [Emanuel convection scheme](https://emanuel.mit.edu/problem-convective-moistening).
+
+The primary use-case is to serve as the under-the-hood moist convection driver
+for [climlab](https://climlab.readthedocs.io/), but it can be used as a
+stand-alone model if you are familiar with [Fortran source code](https://emanuel.mit.edu/FORTRAN-subroutine-convect).
+This is a lightweight wrapper that emulates the Fortran interface as closely as possible.
+The original `convect43c` code is bundled here, courtesy of Kerry Emanuel.
+
+## Installation
+
+Pre-built binaries for many platforms will be available to install from conda-forge. Stay tuned.
+
+For now, please build from source (instructions below).
+
+## Example usage
+
+You can import the modules into a Python session with
+```
+from climlab_rrtmg import rrtmg_lw, rrtmg_sw
+```
+
+The main RRTMG drivers are exposed through
+```
+rrtmg_lw.climlab_rrtmg_lw()
+```
+and
+```
+rrtmg_sw.climlab_rrtmg_sw()
+```
+
+Please see the directory `climlab_rrtmg/tests/` directory in this repository
+for working examples that set up all the necessary input arrays and call the drivers.
+
+## Building from source
+
+Here are instructions to create a build environment (including Fortran compiler)
+with conda/mamba and build using f2py.
+It should be possible to build using other Fortran compilers, but I haven't tested this.
+
+To build *(example for Apple M1 machine, see `./ci/` for other environment files)*:
+```
+mamba create --name convect_build_env python=3.10 --channel conda-forge
+mamba env update --file ./ci/requirements-macos-arm64.yml
+conda activate convect_build_env
+python -m pip install . --no-deps -vv
+```
+
+To run tests, do this from any directory other than the climlab_rrtmg repo:
+```
+pytest -v --pyargs climlab_emanuel_convection
+```
+
+## Version history
+
+Version 0.2 is the first public release (April 2022).
+The Python wrapper code has been extracted from
+[climlab v0.7.13](https://github.com/brian-rose/climlab/releases/tag/v0.7.13).
---
+++ setup.py
@@ -0,0 +1,61 @@
+import os
+
+VERSION = '0.1'
+
+# BEFORE importing setuptools, remove MANIFEST. Otherwise it may not be
+# properly updated when the contents of directories change (true for distutils,
+# not sure about setuptools).
+if os.path.exists('MANIFEST'):
+    os.remove('MANIFEST')
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration
+
+    config = Configuration(None, parent_package, top_path)
+    config.set_options(ignore_setup_xxx_py=True,
+                       assume_default_configuration=True,
+                       delegate_options_to_subpackages=True,
+                       quiet=True)
+    config.add_subpackage('climlab_emanuel_convection')
+    return config
+
+def setup_package():
+    __version__ = VERSION
+    metadata = dict(
+          name='climlab_emanuel_convection',
+          version=__version__,
+          description='Python wrapper for Emanuel moist convection scheme',
+          long_description=readme(),
+          classifiers=[
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python',
+            'Intended Audience :: Education',
+            'Intended Audience :: Science/Research',
+            'Topic :: Scientific/Engineering :: Atmospheric Science',
+          ],
+          keywords='climate modeling modelling model convection earth',
+          url='http://github.com/climlab/climlab-emanuel-convection',
+          author='Brian E. J. Rose',
+          author_email='brose@albany.edu',
+          setup_requires=['numpy'],
+          # install_requires=['numpy','xarray','scipy'],
+          license='MIT',
+    )
+    run_build = True
+
+    # This import is here because it needs to be done before importing setup()
+    # from numpy.distutils, but after the MANIFEST removing and sdist import
+    # higher up in this file.
+    from setuptools import setup
+
+    if run_build:
+        from numpy.distutils.core import setup
+        metadata['configuration'] = configuration
+    setup(**metadata)
+
+if __name__ == '__main__':
+    setup_package()
